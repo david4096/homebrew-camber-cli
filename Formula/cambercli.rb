079f71be7c1898f67264b001e156b51c4bdfdf98
David Steinberg <david@resium.com>
2025-02-04 17:13:56 -0800

    Update cambercli.rb

--- Formula/cambercli.rb
+++ Formula/cambercli.rb
@@ -1,9 +1,14 @@
 class Cambercli < Formula
   desc "CamberCLI - Command-line interface for Camber"
   homepage "https://camber-public-24132152.s3.us-east-2.amazonaws.com/cambercli/download/"
-  url "https://camber-public-24132152.s3.us-east-2.amazonaws.com/cambercli/download/cambercli_0.7.6-dev_Darwin_arm64.tar.gz"
-  version "0.7.6-dev"
-  sha256 "4b9134efef17f5be2bf55ee37c92834a0a1956c70d2197091d05978e689b6f68"
+  if Hardware::CPU.arm?
+    url "https://camber-public-24132152.s3.us-east-2.amazonaws.com/cambercli/download/cambercli_0.7.6-dev_Darwin_arm64.tar.gz"
+    sha256 "4b9134efef17f5be2bf55ee37c92834a0a1956c70d2197091d05978e689b6f68"
+  else
+    url "https://camber-public-24132152.s3.us-east-2.amazonaws.com/cambercli/download/cambercli_0.7.6-dev_Darwin_x86_64.tar.gz"
+    sha256 "997b7c759f5b4cf21560120f784520897cef2f857177ef674d3badf381219649"
+  end  version "0.7.6-dev"
+  
 
   def install
     tarball = Dir["#{cached_download}"].first
